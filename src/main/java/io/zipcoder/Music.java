0f8789baf59c1e9db1b8ac96e8e9d36f0a3d9307
sputnika <sputnika91@gmail.com>
2019-06-01 18:57:16 -0400

    logic..not complete

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,41 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
 
     public Music(String[] playList){
         this.playList = playList;
+
+
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+            if(playList[startIndex]==selection){
+                return 0;
+            }
+
+            for (int x=startIndex; x<playList.length;x++){
+                if(playList[x] == selection){
+                    //store x as variable
+                }
+            }
+            for (int y=startIndex; y>=0;y--){
+                if(playList[y] == selection){
+                    //store y as another variable;
+                }
+            }
+
+            // if x>y == return y
+            // if y > x == return x
+
+
+      return 0;
+
+
+
     }
 }
